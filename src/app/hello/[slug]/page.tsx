8e7dd80b4d5b988932cb9ca260ca595737426e37
Shuntaro Nishizawa <me@shun.technology>
2023-02-25 00:22:57 +0900

    feat: Add test dynamic dynamic import

--- src/app/hello/[slug]/page.tsx
+++ src/app/hello/[slug]/page.tsx
@@ -1,5 +1,23 @@
-export default async function Page(): Promise<JSX.Element> {
-  const { data } = await import("data.json");
+import { notFound } from "next/navigation";
+
+type Params = {
+  slug: string;
+};
+
+type Props = {
+  params: Params;
+};
+
+export default async function Page({ params }: Props): Promise<JSX.Element> {
+  if (!["foo", "bar", "baz"].includes(params.slug)) {
+    notFound();
+  }
+
+  const { data } = await import(`data/${params.slug}.json`);
 
   return <h1>{data}</h1>;
 }
+
+// export async function generateStaticParams(): Promise<Params[]> {
+//   return [{ slug: "foo" }, { slug: "bar" }, { slug: "baz" }];
+// }
